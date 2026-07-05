190aec13c50780e28524b5e53333caf9e9ffc041
GustavoSB3 <161391954+GustavoSB3@users.noreply.github.com>
2026-07-05 15:50:53 -0300

    feat: adiciona modelos iniciais de Pessoa e Transacao

---
+++ backend/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace backend.Models
+{
+    public class Pessoa
+    {
+        public int id  { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Idade { get; set; }
+    }
+}
---
+++ backend/Models/Transacao.cs
@@ -0,0 +1,11 @@
+namespace backend.Models
+{
+    public class Transacao
+    {
+        public int Id  { get; set; }
+        public string Descricao  { get; set; }
+        public decimal Valor  { get; set; }
+        public TipoTransacao Tipo  { get; set; }
+        public int PessoaId { get; set; }
+    }
+}
